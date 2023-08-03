2f51f3b95b12591ac4dc17600ec3a3b76f7cedd4
Abhishek Verma <abhishekvermajsr2000@gmail.com>
2023-08-03 19:29:40 +0530

    revert 2

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(express.json())
 app.use(
   cors({
     origin: [
-      "*",
+      "http://localhost:3000",
       "https://gofood-six.vercel.app"
     ],
     methods: ["GET", "POST", "DELETE", "PATCH"],
