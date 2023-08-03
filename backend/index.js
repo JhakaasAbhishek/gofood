fac01b48a002548aaf17012923eb56d1ca3d598d
Abhishek Verma <abhishekvermajsr2000@gmail.com>
2023-08-03 19:35:14 +0530

    revert 3

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 const app = express()
 const port = 5000
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
